6fb255a58cba8697fed506dbae967b500fe2f0e2
loai abed alhi <loai007thbk@hotmail.com>
2021-07-12 15:10:49 +0300

    fixed first page start

--- APP/ui_main.py
+++ APP/ui_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'uisoospQ.ui'
+## Form generated from reading UI file 'uilsTUcd.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -1461,7 +1461,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(1)
+        self.stackedWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
